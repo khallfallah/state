6a016df833414c76996702269615147c15ad6bb9
khallfallah <wafakhallfallah75@gmail.com>
2023-04-28 07:47:55 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+class App extends React.Component {
+  // eslint-disable-next-line react/require-render-return
+  state = {
+    fullName: "khallfallah wafa",
+    bio: "systeme embarqué ",
+    imgSrc: "vvvv",
+    profession: "student ",
+    timer:0,
+    display: true
+  }
+  disbutton = () => {
+    this.setState({ display: !this.state.display })
+  }
+  componentDidMount() {
+    console.log("Counter Mounted");
+    setInterval(() => {this.setState({timer:this.state.timer+1})}, 1000);
+}
+  render() {
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+          <button onClick={this.disbutton}>{this.state.display ? "Hide" : "Show"}</button>
+          {this.state.display ?
+            <div>
+              <span >profile </span>
+              
+              <span className='block'>fullName: {this.state.fullName} </span>
+              <span className='block'> bio : {this.state.bio}</span>
+              <span className='block'>photo :{this.state.imgSrc} </span>
+              <span className='block'>profession: {this.state.profession} </span>
+              <h4>Counter Timer:{this.state.timer}</h4>
+            </div>
+            : null}
+        </div>
     </div>
   );
 }
